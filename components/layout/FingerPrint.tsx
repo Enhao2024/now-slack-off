c631b77f5b09fd369ece9b7272b20d1878f0e01d
enhao <efen866@aucklanduni.ac.nz>
2024-07-14 23:53:47 +1200

    feat: integrated AWS backend to submit user fingerprint

--- components/layout/FingerPrint.tsx
+++ components/layout/FingerPrint.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
+import axios from "axios";
 
 const FingerPrint = () => {
 
   getCurrentBrowserFingerPrint().then((fingerprint) => {
-
-    // todo: commit fingerprint to server
-
+    const data = {
+      fingerprint: fingerprint.toString(),
+    };
+    axios.post('https://d2n05hlrnagjc.cloudfront.net/prod/user/visit', data)
   })
 
   return null;
