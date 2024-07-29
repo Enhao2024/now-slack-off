7b2f1e645f34395b907fc0b8d3dc9539fed0a4c3
Enhao2024 <fenge667@gmail.com>
2024-07-29 23:13:25 +1200

    NSO-4 removed server side request (#7)
    Co-authored-by: enhao <efen866@aucklanduni.ac.nz>

--- components/layout/FingerPrint.tsx
+++ components/layout/FingerPrint.tsx
@@ -1,11 +1,17 @@
 'use client'
+import { getBaseUrl } from "@/utils/GeneralUtils";
 import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
 import axios from "axios";
 
 const FingerPrint = () => {
 
   getCurrentBrowserFingerPrint().then(async (fingerprint) => {
-    await axios.post('/api/visit', { fingerprint });
+    const baseUrl = getBaseUrl();
+    try {
+      await axios.post(`${baseUrl}/user/visit`, { fingerprint: fingerprint.toString() });
+    } catch (error) {
+      console.log(`Request Failed for ${baseUrl}/user/visit`);
+    }
   })
 
   return null;
