5bcdbc6c95de1459401e2d0e7f6e7ac6bcce9621
Enhao2024 <fenge667@gmail.com>
2024-08-02 22:41:27 +1200

    NSO-10 request body encryption (#8)
    Co-authored-by: enhao <efen866@aucklanduni.ac.nz>

--- components/layout/FingerPrint.tsx
+++ components/layout/FingerPrint.tsx
@@ -2,13 +2,35 @@
 import { getBaseUrl } from "@/utils/GeneralUtils";
 import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
 import axios from "axios";
+import { publicEncrypt } from 'crypto';
+import { Buffer } from 'buffer';
+
+interface RequestBody {
+  fingerprint: string;
+}
+
+const publicKey: string = process.env.REQUEST_BODY_KEY || '';
+
+function encryptWithPublicKey(publicKey: string, data: RequestBody) {
+  const buffer = Buffer.from(JSON.stringify(data));
+  const uint8Array = new Uint8Array(buffer)
+  const encrypted = publicEncrypt(
+    publicKey,
+    uint8Array
+  );
+  return encrypted.toString('base64');
+}
 
 const FingerPrint = () => {
 
   getCurrentBrowserFingerPrint().then(async (fingerprint) => {
     const baseUrl = getBaseUrl();
+    const requestBody: RequestBody = { fingerprint: fingerprint.toString() };
+    const encryptedBody = encryptWithPublicKey(publicKey, requestBody);
     try {
-      await axios.post(`${baseUrl}/user/visit`, { fingerprint: fingerprint.toString() });
+      await axios.post(`${baseUrl}/user/visit`, {
+        body: encryptedBody
+      });
     } catch (error) {
       console.log(`Request Failed for ${baseUrl}/user/visit`);
     }
