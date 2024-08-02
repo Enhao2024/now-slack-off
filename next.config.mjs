976561519b625e67415902ec69bc7b889320fd41
enhao <efen866@aucklanduni.ac.nz>
2024-08-02 22:39:37 +1200

    NSO-10 request body encryption

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,8 @@ const nextConfig = {
     PROD_API_URL: process.env.PROD_API_URL,
     DEV_API_URL: process.env.DEV_API_URL,
     STATIC_LIB_URL: process.env.STATIC_LIB_URL,
+    // public key
+    REQUEST_BODY_KEY: process.env.REQUEST_BODY_KEY,
   },
 };
 
