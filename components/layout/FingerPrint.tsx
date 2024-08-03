42792d7b8846fcbb3b5d9c1c6b813d6509c3d227
Enhao2024 <fenge667@gmail.com>
2024-08-03 14:13:54 +1200

    NSO-2 frontend debounce using radash (#9)
    Co-authored-by: enhao <efen866@aucklanduni.ac.nz>

--- components/layout/FingerPrint.tsx
+++ components/layout/FingerPrint.tsx
@@ -4,6 +4,9 @@ import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
 import axios from "axios";
 import { publicEncrypt } from 'crypto';
 import { Buffer } from 'buffer';
+import { debounce } from 'radash'
+import { useRef, useEffect } from "react";
+
 
 interface RequestBody {
   fingerprint: string;
@@ -11,32 +14,43 @@ interface RequestBody {
 
 const publicKey: string = process.env.REQUEST_BODY_KEY || '';
 
-function encryptWithPublicKey(publicKey: string, data: RequestBody) {
-  const buffer = Buffer.from(JSON.stringify(data));
-  const uint8Array = new Uint8Array(buffer)
-  const encrypted = publicEncrypt(
-    publicKey,
-    uint8Array
-  );
-  return encrypted.toString('base64');
-}
-
 const FingerPrint = () => {
 
-  getCurrentBrowserFingerPrint().then(async (fingerprint) => {
-    const baseUrl = getBaseUrl();
-    const requestBody: RequestBody = { fingerprint: fingerprint.toString() };
-    const encryptedBody = encryptWithPublicKey(publicKey, requestBody);
-    try {
-      await axios.post(`${baseUrl}/user/visit`, {
-        body: encryptedBody
-      });
-    } catch (error) {
-      console.log(`Request Failed for ${baseUrl}/user/visit`);
-    }
-  })
+  // encrypt func
+  const encryptWithPublicKey = (publicKey: string, data: RequestBody) => {
+    const buffer = Buffer.from(JSON.stringify(data));
+    const uint8Array = new Uint8Array(buffer)
+    const encrypted = publicEncrypt(
+      publicKey,
+      uint8Array
+    );
+    return encrypted.toString('base64');
+  }
+
+  // request func
+  const request = () => {
+    getCurrentBrowserFingerPrint().then(async (fingerprint) => {
+      const baseUrl = getBaseUrl();
+      const requestBody: RequestBody = { fingerprint: fingerprint.toString() };
+      const encryptedBody = encryptWithPublicKey(publicKey, requestBody);
+      try {
+        await axios.post(`${baseUrl}/user/visit`, {
+          body: encryptedBody
+        });
+      } catch (error) {
+        console.log(`Request Failed for ${baseUrl}/user/visit`);
+      }
+    })
+  }
+
+  // debounce func
+  const debouncedFunctionRef = useRef(debounce({ delay: 5000 }, request));
+
+  useEffect(() => {
+    debouncedFunctionRef.current();
+  }, []);
 
   return null;
-}
+};
 
-export default FingerPrint
+export default FingerPrint;
