1c4d9a0de178978155c98e48fd946fd28fe286af
enhao <efen866@aucklanduni.ac.nz>
2024-08-10 17:08:44 +1200

    NSO-14 integrated AWS SES

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,36 +1,11 @@
+import Email from '@/components/email/Email'
 import Entrance from '@/components/layout/Entrance'
 import React from 'react'
 
 const page = () => {
   return (
     <Entrance>
-      <div className="hero">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left px-8">
-            <h1 className="text-5xl font-bold">Any Thoughts?</h1>
-            <p className="py-6 font-semibold">We value every ridiculous thought! Talk to us about your brainstorm and help promoting our playful community!</p>
-          </div>
-          <div className="card shrink-0 w-full max-w-md shadow-2xl bg-base-100">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Content</span>
-                </label>
-                <textarea className="textarea textarea-primary" placeholder="Content"></textarea>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Send</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Email/>
     </Entrance>
   )
 }
