ff29c2218f13a2838340649a8a0ca565e207c976
enhao <efen866@aucklanduni.ac.nz>
2024-09-03 23:59:32 +1200

    NSO-15 updated personal CV page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,25 +9,25 @@ const page = () => {
     <Entrance custom='my-10 lg:my-20'>
       <div className="flex flex-col justify-between items-center">
         <div className="flex flex-col justify-between items-center space-y-16">
-          <h1 className="text-3xl font-bold">Just! For! Fun!</h1>
-          <p className="font-semibold leading-relaxed text-center max-w-md">A garage project aiming to help <del>slacking off</del> and entertaining your smoko time, we are not encourging slack-off seriously but wish you may chill out here <strong>Especially After A Shitty Day</strong></p>
+          <h1 className="text-3xl font-bold">What Is This</h1>
+          <p className="font-semibold leading-relaxed text-center max-w-md">A garage project aiming to help <del>slacking off</del> and entertaining your smoko time, we are not encourging slack-off seriously but wish you may chill out here</p>
           <MenuLink menuName={MenuTitle.CONTACT}>
             <button className="btn btn-primary w-64">Contact Us</button>
           </MenuLink>
 
         </div>
         <div className="divider m-16"></div>
-        <h1 className="text-3xl font-bold">Contributors</h1>
+        <h1 className="text-3xl font-bold">Who Are We</h1>
         <VerticalFeatureRow
           title="MrPilot: Full Stack Developer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+          description="Code is Art. Never confined to any boundaries, always pursuing fancy thoughts. Enjoying weekends with coffee, coding, and occasionally a fishing trip."
           image="me.jpg"
           imageAlt="First feature alt text"
           reverse
         />
         <VerticalFeatureRow
           title="Ooooouch: UI/UX Designer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+          description="Cuteness is the ultimate productivity booster. Obsessed with all things fluffy and four-legged, and believes nothing beats a session of Genshin Impact after a day of creative brainstorming."
           image="ouch.jpeg"
           imageAlt="First feature alt text"
         />
